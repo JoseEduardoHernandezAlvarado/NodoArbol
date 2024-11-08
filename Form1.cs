6c01ad22bbefd371b9101f7c0a954c73c0adcde9
JOSE JOEL MARTINEZ LOPEZ <L23212648@tijuana.tecnm.mx>
2024-11-08 07:07:32 -0800

    Agregado resto del código (faltan métodos)

--- Form1.cs
+++ Form1.cs
@@ -21,7 +21,48 @@ namespace NodoArbol
         int Dato = 0;
         int cont = 0;
 
-        Nodo_Arbol mi_Arbol = new NodoArbol(null);
+        Nodo_Arbol mi_Arbol = new Nodo_Arbol(null); //Creación del objeto arbol
+        Graphics g; // Definición del objeto grafico
+
+        //Evento del formulario que permitira dibujar el arbol binario
+        private void Form1_Paint(object sender, PaintEventArgs en)
+        {
+            en.Graphics.Clear(this.BackColor);
+            en.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+            en.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            g = en.Graphics;
+
+            mi_Arbol.DibujarArbol(g, this.Font, Brushes.Blue, Brushes.White, Pens.Black, Brushes.White);
+        }
+
+        //Evento que permitira insertar un nodo al arbol (código del boton "Insertar nodo" del formulario mostrado en la figura)
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            if(txtDato.Text == "")
+            {
+                MessageBox.Show("Debe ingresar un valor.");
+            }
+            else
+            {
+                Dato = int.Parse(txtDato.Text);
+                if(Dato <= 0 || Dato >= 100)
+                {
+                    MessageBox.Show("Debe ingresar un valor entre 1 y 99", "Error de ingreso");
+                }
+                else
+                {
+                    mi_Arbol.Insertar(Dato);
+
+                    txtDato.Clear;
+                    txtDarto.Focus;
+
+                    cont++;
+
+                    Refresh();
+                    Refresh();
+                }
+            }
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
